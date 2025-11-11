6b51a480c247c68248dc542d01d2e0334ec47874
vortizdev <karberusx2@gmail.com>
2025-11-10 21:57:24 -0500

    Add count_vowels_dict function and tests
    Introduces count_vowels_dict to return a dictionary of vowel counts in a string. Updates tests to cover the new function, including cases for all vowels, no vowels, and invalid input types.

--- src/count_vowels.py
+++ src/count_vowels.py
@@ -1,6 +1,6 @@
 
 def count_vowels(string: str) -> int:
-    """ Count the total number of vowels in a list of strings. """
+    """ Count the total number of vowels in a string. """
     if not isinstance(string, str):
         raise TypeError("Input must be a string.")
     
@@ -10,3 +10,22 @@ def count_vowels(string: str) -> int:
     total_vowels += sum(1 for char in string if char in vowels)
     
     return total_vowels
+    
+
+def count_vowels_dict(_string: str) -> dict:
+    """ 
+    Count the total number of vowels in a string and return a dict showing how many 
+    times each vowel appears.
+    """
+    if not isinstance(_string, str):
+        raise TypeError("Input must be a string.")
+    
+    string = _string.lower()
+    vowels = set("aeiou")
+    vowel_count = {'a': 0, 'e': 0, 'i': 0, 'o': 0, 'u': 0,}
+    
+    for char in string:
+        if char in vowels:
+            vowel_count[f"{char}"] += 1
+                
+    return vowel_count
--- tests/test_count_vowels.py
+++ tests/test_count_vowels.py
@@ -1,4 +1,4 @@
-from src.count_vowels import count_vowels
+from src.count_vowels import count_vowels, count_vowels_dict
 
 def test_count_vowels_basic():
     assert count_vowels("hello") == 2
@@ -18,6 +18,16 @@ def test_count_vowels_mixed_case():
 def test_invalid_input_type():
     try:
         count_vowels(12345)
+        count_vowels_dict(12345)
     except TypeError as e:
         assert str(e) == "Input must be a string."
+        
+def test_count_vowels_dict():
+    assert count_vowels_dict("helloo") == {"a": 0, "e": 1, "i": 0, "o": 2, "u": 0}
+    
+def test_count_vowels_dict_all_vowels():
+    assert count_vowels_dict("aeiouAEIOU") == {"a": 2, "e": 2, "i": 2, "o": 2, "u": 2}
+
+def test_count_vowels_dict_no_vowels():
+    assert count_vowels_dict("rythm") == {"a": 0, "e": 0, "i": 0, "o": 0, "u": 0}
     
